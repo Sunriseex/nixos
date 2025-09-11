3fdc42fbe1c9882e0b5b2548dc8ff62464949183
Sunriseex <norealpwnz@gmail.com>
2025-09-11 04:50:33 +0300

    feat: firefox extansions and more privacy options

--- modules/home-manager/apps/firefox.nix
+++ modules/home-manager/apps/firefox.nix
@@ -7,7 +7,7 @@
     profiles.default = {
       settings = {
         "browser.newtabpage.activity-stream.feeds.topsites" = false;
-        "browser.tabs.inTitlebar" = 0;
+        # "browser.tabs.inTitlebar" = 0;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "browser.startup.page" = 0;
         "browser.startup.homepage" = "about:blank";
@@ -22,20 +22,49 @@
     policies = {
       DisableTelemetry = true;
       DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableFirefoxAccounts = true;
-      DisableFirefoxScreenshots = true;
-      OverrideFirstRunPage = true;
-      OverridePostUpdatePage = true;
-      DontCheckDefaultBrowser = true;
-      SearchBar = "unified";
-
       EnableTrackingProtection = {
         Value = true;
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
       };
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = "";
+      OverridePostUpdatePage = "";
+      DontCheckDefaultBrowser = true;
+      DisplayBookmarksToolbar = "always";
+      DisplayMenuBar = "default-off";
+      SearchBar = "unified";
+      ExtensionSettings = {
+        "*".installation_mode = "blocked";
+        "uBlock0@raymondhill.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "jid1-MnnxcxisBPnSXQ@jetpack" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "firefox@tampermonkey.net" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/tampermonkey/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "sponsorBlocker@ajay.app" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "keepassxc-browser@keepassxc.org" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/keepassxc-browser/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{506e023c-7f2b-40a3-8066-bc5deb40aebe}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/gesturefy/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+
     };
 
   };
