6bcb13ee49fb6d75cd05bdef6ea9ffaa56a4eef0
Sunriseex <norealpwnz@gmail.com>
2025-09-11 03:37:06 +0300

    feat: change firefox config

--- modules/home-manager/apps/firefox.nix
+++ modules/home-manager/apps/firefox.nix
@@ -3,5 +3,42 @@
 {
   programs.firefox = {
     enable = true;
+
+    /*
+      profile.default = {
+        settings = {
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.tabs.inTitlebar" = 0;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.startup.page" = 0;
+          "browser.startup.homepage" = "about:blank";
+          "browser.newtabpage.activity-stream.showSponsored" = false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.newtabpage.activity-stream.default.sites" = "";
+          "browser.urlbar.suggest.calculator" = true;
+        };
+
+      };
+    */
+
+    policies = {
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxScreenshots = true;
+      OverrideFirstRunPage = true;
+      OverridePostUpdatePage = true;
+      DontCheckDefaultBrowser = true;
+      SearchBar = "unified";
+
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
+
   };
 }
