4c5ce1f058672bc694b74fd7e548f0d401da1699
Sunriseex <norealpwnz@gmail.com>
2025-09-11 04:17:36 +0300

    feat: update firefox config

--- modules/home-manager/apps/firefox.nix
+++ modules/home-manager/apps/firefox.nix
@@ -4,22 +4,20 @@
   programs.firefox = {
     enable = true;
 
-    /*
-      profile.default = {
-        settings = {
-          "browser.newtabpage.activity-stream.feeds.topsites" = false;
-          "browser.tabs.inTitlebar" = 0;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "browser.startup.page" = 0;
-          "browser.startup.homepage" = "about:blank";
-          "browser.newtabpage.activity-stream.showSponsored" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.newtabpage.activity-stream.default.sites" = "";
-          "browser.urlbar.suggest.calculator" = true;
-        };
-
+    profiles.default = {
+      settings = {
+        "browser.newtabpage.activity-stream.feeds.topsites" = false;
+        "browser.tabs.inTitlebar" = 0;
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.startup.page" = 0;
+        "browser.startup.homepage" = "about:blank";
+        "browser.newtabpage.activity-stream.showSponsored" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+        "browser.newtabpage.activity-stream.default.sites" = "";
+        "browser.urlbar.suggest.calculator" = true;
       };
-    */
+
+    };
 
     policies = {
       DisableTelemetry = true;
