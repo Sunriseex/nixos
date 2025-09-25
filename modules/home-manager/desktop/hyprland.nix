ec1dcc303c70b1b911b4c2cc040762b17ff2b765
Sunriseex <norealpwnz@gmail.com>
2025-09-25 21:59:09 +0300

    refactor: remove unused gestures configuration from hyprland settings

--- modules/home-manager/desktop/hyprland.nix
+++ modules/home-manager/desktop/hyprland.nix
@@ -157,10 +157,6 @@
         numlock_by_default = true;
       };
 
-      gestures = {
-        workspace_swipe = false;
-      };
-
       device = {
         name = "epic-mouse-v1";
         sensitivity = -0.5;
