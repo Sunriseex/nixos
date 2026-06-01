130fe694270278918c7f75d53989b53c9ad65349
Sunriseex <norealpwnz@gmail.com>
2026-06-01 07:16:37 +0300

    fix: run protonup-qt without proxy env

--- hosts/desktop-pc/system-modules/gaming.nix
+++ hosts/desktop-pc/system-modules/gaming.nix
@@ -1,5 +1,28 @@
 { pkgs, ... }:
 
+let
+  protonup-qt-unproxied = pkgs.symlinkJoin {
+    name = "protonup-qt-unproxied";
+    paths = [ pkgs.protonup-qt ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram "$out/bin/protonup-qt" \
+        --unset http_proxy \
+        --unset https_proxy \
+        --unset ftp_proxy \
+        --unset rsync_proxy \
+        --unset all_proxy \
+        --unset no_proxy \
+        --unset HTTP_PROXY \
+        --unset HTTPS_PROXY \
+        --unset FTP_PROXY \
+        --unset RSYNC_PROXY \
+        --unset ALL_PROXY \
+        --unset NO_PROXY
+    '';
+  };
+in
+
 {
   programs.steam = {
     enable = true;
@@ -21,6 +44,6 @@
 
   environment.systemPackages = with pkgs; [
     mangohud
-    protonup-qt
+    protonup-qt-unproxied
   ];
 }
